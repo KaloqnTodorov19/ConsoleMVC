517085bcada22d7936c2b9c76b0e95e4e105a9d4
Kaloqn Todorov <k.todorov2019@pmg-pernik.com>
2023-02-13 09:00:40 +0200

    added zad

---
+++ ConsoleMVCc/ConsoleMVC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleMVC.Views;
+using ConsoleMVC.Model;
+using ConsoleMVC.Controllers;
+
+namespace ConsoleMVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TipCalculatorController tipCalculatorController = new TipCalculatorController();
+        }
+    }
+}
